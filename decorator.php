08f9788dac097edda6f0d6b88f8ab69ac206b3d9
jNullj <iontankatchker@gmail.com>
2016-07-01 15:23:58 +0300

    Added cycled text convertor

--- decorator.php
+++ decorator.php
@@ -1,7 +1,32 @@
 <?php
 
 class Decorator {
-
+  private $upper;
+  private $lower;
+  private $nums;
+  private $origin;
+  
+  void __construct(){
+    $upper = range('A', 'Z');
+    $lower = range('a', 'z');
+    $nums = range('0','9');
+    $origin = $lower + $upper + $nums;
+  }
+  
+  public function cycle($text){
+    $id_upper = range(9398,9423);
+    $id_lower = range(9324,9449);
+    $id_nums = [9450] + range(9312,9320);
+    $id = $id_lower + $id_upper + $id_nums;
+    $map = [];
+    foreach($id as $val)
+    {
+      $myval = "\u{$val}";
+      $myval = json_decode($myval);
+      $map = $map + [$myval];
+    }
+    return str_replace($original, $map, $text);
+  }
 }
 
 ?>
